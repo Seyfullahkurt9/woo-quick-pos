d851edf96a599cc49546c900af997aad201d2df3
Seyfullah Kurt <seyfullahkurt9@gmail.com>
2026-08-05 12:46:57 +0300

    refactor(constants): simplify HIZLI_KASA_VERSION reading using WP get_file_data API

--- includes/constants.php
+++ includes/constants.php
@@ -8,7 +8,15 @@ if (!defined('HIZLI_KASA_BOOT_TIME')) {
     define('HIZLI_KASA_BOOT_TIME', microtime(true));
 }
 
+<<<<<<< HEAD
 define('HIZLI_KASA_VERSION', '12.53.0');
+=======
+// Sürüm Sabiti (Ana hizli-kasa.php başlığından otomatik okunur)
+if (!defined('HIZLI_KASA_VERSION')) {
+    $hk_header = function_exists('get_file_data') ? get_file_data(dirname(__DIR__) . '/hizli-kasa.php', ['Version' => 'Version']) : [];
+    define('HIZLI_KASA_VERSION', !empty($hk_header['Version']) ? $hk_header['Version'] : '12.53.0');
+}
+>>>>>>> c7cb9b4 (refactor(constants): simplify HIZLI_KASA_VERSION reading using WP get_file_data API)
 
 // Harici İndirme ve Servis URL'leri
 define('HIZLI_KASA_HELPER_DOWNLOAD_URL', 'https://github.com/Seyfullahkurt9/web-print-helper/releases/latest/download/web-print-helper.exe');
