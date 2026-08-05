896b4adfb1b28905f9e60a646b880a0187e44d94
Seyfullah Kurt <seyfullahkurt9@gmail.com>
2026-08-05 13:49:42 +0300

    fix(print-manager): improve error guidance for re-pairing and bump version to 12.54.1

--- includes/constants.php
+++ includes/constants.php
@@ -11,6 +11,7 @@ if (!defined('HIZLI_KASA_BOOT_TIME')) {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 define('HIZLI_KASA_VERSION', '12.53.0');
 =======
 // Sürüm Sabiti (Ana hizli-kasa.php başlığından otomatik okunur)
@@ -25,6 +26,9 @@ define('HIZLI_KASA_VERSION', '12.53.0');
 =======
 define('HIZLI_KASA_VERSION', '12.54.0');
 >>>>>>> d191479 (feat(stock): add zero-cost regular & sale price badges to admin stock management list, bump version to 12.54.0)
+=======
+define('HIZLI_KASA_VERSION', '12.54.1');
+>>>>>>> 61ead93 (fix(print-manager): improve error guidance for re-pairing and bump version to 12.54.1)
 
 // Harici İndirme ve Servis URL'leri
 define('HIZLI_KASA_HELPER_DOWNLOAD_URL', 'https://github.com/Seyfullahkurt9/web-print-helper/releases/latest/download/web-print-helper.exe');
